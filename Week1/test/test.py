10b1466892bd81383b8d5f07cf2c1b6d5c256f81
abhi2596 <randy2596@gmail.com>
2022-07-05 18:12:14 -0500

    made changes to test.py file

--- Week1/test/test.py
+++ Week1/test/test.py
@@ -1,5 +1,5 @@
-import sys,os
-sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+# import sys,os
+# sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 from Week1.scripts.add import load_json
 
 
